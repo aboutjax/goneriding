a33bd4dd03be2ef84d3abde642232ab7a9c0abe3
Jacky Lee <aboutjax1990@gmail.com>
2018-12-25 14:26:12 +1300

    update

--- src/components/image.js
+++ src/components/image.js
@@ -17,9 +17,9 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        placeholderImage: file(relativePath: { eq: "test.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 300) {
+            fluid(maxWidth: 4000) {
               ...GatsbyImageSharpFluid
             }
           }
--- src/templates/ride.js
+++ src/templates/ride.js
@@ -2,6 +2,13 @@ import React from 'react'
 import RideLayout from '../components/ride-layout'
 import { graphql } from 'gatsby'
 import Map from '../components/map'
+import rehypeReact from 'rehype-react'
+import Image from '../components/image'
+
+const renderAst = new rehypeReact({
+  createElement: React.createElement,
+  components: { 'js-image': Image },
+}).Compiler
 
 export default ({ data }) => {
   console.log(data)
@@ -10,11 +17,8 @@ export default ({ data }) => {
   return (
     <div className="flex">
       <RideLayout>
-        <div className="pa4">
-          <div
-            className="mw6 center"
-            dangerouslySetInnerHTML={{ __html: post.html }}
-          />
+        <div className="pa4 ride-post">
+          <div className="mw6 center">{renderAst(post.htmlAst)}</div>
         </div>
       </RideLayout>
       <div className="w-50 top-0 bottom-0 right-0">
@@ -27,7 +31,7 @@ export default ({ data }) => {
 export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
-      html
+      htmlAst
       fields {
         strava_id
       }
