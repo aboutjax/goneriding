220055794ea22afeb0e57c591aad4be310a47067
Jacky Lee <aboutjax1990@gmail.com>
2019-01-02 21:42:02 +1300

    add emoji to header

--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,9 @@ const Header = ({ siteTitle }) => (
   <div className="c-header near-black pv4 ph2 ph3-l flex justify-between items-center flex-none">
     <h2 className="ph3 lh-solid fw9 dim f3-l f4 serif c-header__logo">
       <Link to="/" className="near-black no-underline ttc link">
+        <span role="img" aria-label="mountain">
+          🏔{' '}
+        </span>
         {siteTitle}
       </Link>
     </h2>
