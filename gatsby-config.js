5dde745a8f4241989c745a6a8d7cd4c89e4bda2e
Jacky Lee <aboutjax1990@gmail.com>
2019-01-02 19:23:36 +1300

    update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,12 +53,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Goneriding`,
+        short_name: `Goneriding`,
         start_url: `.`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        background_color: `#4E4E4E`,
+        theme_color: `#4E4E4E`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
