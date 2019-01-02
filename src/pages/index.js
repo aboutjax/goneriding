87ce437d7a973f53e1358556245390a931973281
Jacky Lee <aboutjax1990@gmail.com>
2019-01-02 19:59:19 +1300

    fix css load place

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Img from 'gatsby-image'
-import '../styles/index.scss'
 
 import IndexLayout from '../components/index-layout'
 import SEO from '../components/seo'
