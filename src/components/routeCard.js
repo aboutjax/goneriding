96dce0988556ac25d0308f36b83d648e3f1231fe
Jacky Lee <aboutjax1990@gmail.com>
2019-01-07 17:11:56 +1300

    add routecard

---
+++ src/components/routeCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class RouteCard extends Component {
+  render() {
+    return (
+      <div className="">
+        <p>route card</p>
+      </div>
+    )
+  }
+}
+
+export default RouteCard
