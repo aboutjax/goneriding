a8182dc368a1db11ca5b6c56e58afd66d5b1e339
Jacky Lee <aboutjax1990@gmail.com>
2019-01-10 16:00:20 +1300

    edit userscaleable in html head

--- src/html.js
+++ src/html.js
@@ -10,8 +10,9 @@ export default class HTML extends React.Component {
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no"
           />
+
           {this.props.headComponents}
           <noscript>
             <p>Please enable javascript!</p>
