568ef816e755a8d0e4dd09a7140a7cdafacffbe8
Jacky Lee <aboutjax1990@gmail.com>
2019-01-28 00:10:57 +0800

    fix mapbox gatsby build error node

--- gatsby-node.js
+++ gatsby-node.js
@@ -54,3 +54,19 @@ exports.createPages = ({ graphql, actions }) => {
     })
   })
 }
+
+
+exports.onCreateWebpackConfig = ({ actions, stage }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /mapbox-gl/,
+            use: ['null-loader']
+          },
+        ],
+      }
+    })
+  }
+};
